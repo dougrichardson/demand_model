e40e829818e9bd2f58a7b7651f94fc6af279f7b1
Doug Richardson <54010293+dougrichardson@users.noreply.github.com>
2023-10-17 14:16:32 +1100

    Fixed bash script

--- run_multiple_random_forest.sh
+++ run_multiple_random_forest.sh
@@ -12,11 +12,15 @@
 # # Load module, always specify version number.
 # module load python
 # python -m venv --system-site-packages /g/data/w42/dr6273/apps/conda/envs/ml_env
-conda activate ml_env
+#conda activate pangeo_ML
 
 # # Run Python applications
-python run_random_forest.py /g/data/w42/dr6273/work/projects/Aus_energy/ daily_demand_2010-2020_stl.nc NEM NSW False False 0 pop_dens_mask is_weekend 2010 2011 2012 2012#> $PBS_JOBID.log
+#python run_random_forest.py /g/data/w42/dr6273/work/projects/Aus_energy/ daily_demand_2010-2020_stl.nc NEM NSW False False 0 pop_dens_mask is_weekend 2010 2011 2012 2012
+#> $PBS_JOBID.log
 
-echo "TEST"
+conda run -n pangeo_ML python3 run_random_forest.py /g/data/w42/dr6273/work/projects/Aus_energy/ daily_demand_2010-2020_stl.nc NEM NSW False False 0 pop_dens_mask is_weekend 2010 2011 2012 2012
+
+#echo "TEST"
  
 # # Deactivate virtual environment, if any.
+#conda deactivate
