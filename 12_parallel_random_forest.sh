cd64c6248ab667cfb347aeba296d5f629461581f
Doug Richardson <54010293+dougrichardson@users.noreply.github.com>
2024-05-06 09:32:46 +1000

    Redo is_weekend runs to include workday flag

--- 12_parallel_random_forest.sh
+++ 12_parallel_random_forest.sh
@@ -14,7 +14,8 @@
 module load parallel
 conda activate pangeo_ML
 
-INPUTS=/g/data/w42/dr6273/work/demand_model/parallel_inputs.txt
+# INPUTS=/g/data/w42/dr6273/work/demand_model/parallel_inputs.txt
+INPUTS=/g/data/w42/dr6273/work/demand_model/parallel_inputs_redo_isweekend.txt
 
 # Declare an empty array. This array will store the contents of the input file.
 declare -a array
